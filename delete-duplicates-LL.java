7461d0169dc9b0aef23dec81ffdd717ef5d5252c
Maksym Luhin <luhinm@tcd.ie>
2026-01-25 23:52:27 +0000

    solved linked list problem

---
+++ delete-duplicates-LL.java
@@ -0,0 +1,23 @@
+ public ListNode deleteDuplicates(ListNode head) {
+        ListNode localHead = head;
+        ListNode currentNode = head;
+
+        while(currentNode != null && currentNode.next != null){
+            if(currentNode.val == currentNode.next.val) {
+                currentNode.next = currentNode.next.next;
+            }
+
+            if(currentNode.next == null) {
+                break;
+            }
+            //check if the next value is different
+            else if(currentNode.val != currentNode.next.val){
+                currentNode = currentNode.next;
+            }   
+            else {
+                continue;
+            }
+        }
+
+        return localHead;
+    }
