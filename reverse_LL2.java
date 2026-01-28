2fea67323714531005f921f8a47e4d6edc749c3c
Maksym Luhin <malslou777@gmail.com>
2026-01-28 23:38:10 +0000

    check if activity is shown

---
+++ reverse_LL2.java
@@ -0,0 +1,17 @@
+public class reverse_LL2 {
+    public class ListNode {
+     int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        int currentNodeCount = 0;
+        
+        System.out.print("bla blba bla");
+        
+        return head;
+    }
+}
